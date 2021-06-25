fb9902e1fcff117dd8cc31e71dfd5a006c5970de
yukan97 <yuliia.kanarovska@gmail.com>
2021-06-25 16:24:15 +0300

    fix: OSError: [Errno 22] Invalid argument

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -44,8 +44,8 @@ def go(args):
     run.config.update(args)
 
     # Get the Random Forest configuration and update W&B
-    logger.info(r'%s' %args.rf_config)
-    with open(r'%s' %args.rf_config) as fp:
+    logger.info('%s' %args.rf_config)
+    with open('%s' %args.rf_config) as fp:
         rf_config = json.load(fp)
     run.config.update(rf_config)
 
