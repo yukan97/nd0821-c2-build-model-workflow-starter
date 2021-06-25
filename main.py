8a830dae52d011c0249eb1695b28379d7c13b2e9
yukan97 <yuliia.kanarovska@gmail.com>
2021-06-25 15:30:36 +0300

    fix: searching for source of problem
    run.py gives an error: "the following arguments are required: --output_artifact", eventhought it was passed

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def go(config: DictConfig):
                     "stratify_by": config['modeling']['stratify_by'],
                     "rf_config": rf_config,
                     "max_tfidf_features": config['modeling']['max_tfidf_features'],
-                    "output_artifact": "random_forest_export"
+                    "output_artifact": "random_forest_export",
                 },
             )
 
